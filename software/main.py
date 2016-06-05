699f12d507cda4ae938bba3c32d104f2f24fed90
Christian Assing <chris@ca-net.org>
2016-06-05 10:02:24 +0200

    pyq2 software v0.1

--- software/main.py
+++ software/main.py
@@ -12,46 +12,31 @@ TIMEZONE_OFFSET = 2
 OFF = Matrix.off
 
 # init spi interface
-spi = SPI(0, mode=SPI.MASTER, bits=32, pins=('GP14', 'GP16', 'GP15'))
-latch = Pin('GP22', mode=Pin.OUT)
+latch = Pin('GP13', mode=Pin.OUT)
 latch.value(0)
+spi = SPI(0, mode=SPI.MASTER, bits=32, pins=('GP14', 'GP16', 'GP15'))
 trigger = Pin('GP5', mode=Pin.OUT)
 trigger.value(0)
 
-# all off
-spi.write(OFF)
-# update LEDs
-latch.value(1)
-latch.value(0)
-
 # set internal clock
 rtc = settime(timezone_offset=TIMEZONE_OFFSET)
-
-
-def t(timer):
-    trigger.toggle()
-    print("timer")
 
 
 def main():
     global rtc
 
     try:
-        tim = Timer(1, mode=Timer.ONE_SHOT, width=32)
+        year, month, day, hour, minute, second, *_ = rtc.now()
+        # initial trigger timer
+        timer = Timer(3, mode=Timer.PERIODIC, width=32)
+        # timer_channel = timer.channel(Timer.A | Timer.B, period=(60 - second) * 1000000)
+        timer_channel = timer.channel(Timer.A | Timer.B, period=30000000)
+        timer_channel.irq(handler=lambda t: trigger.toggle(), trigger=Timer.TIMEOUT)
 
         while True:
-            year, month, day, hour, minute, second, *_ = rtc.now()
-            timer = tim.channel(Timer.A | Timer.B, period=(60 - second) * 1000000)
-            timer.irq(handler=t, trigger=Timer.TIMEOUT)
-
-            print(hour, minute, second)
-            if hour == 4 and minute == 0 and second == 0:
-                rtc = settime(timezone_offset=TIMEZONE_OFFSET)
-
             leds = clock2matrix(hour=hour, minute=minute).columns
 
             # led matrix multiplexing
-            # while True:
             current_trigger = trigger.value()
             while trigger.value() == current_trigger:
                 for col in leds:
@@ -66,8 +51,16 @@ def main():
                     # update LEDs
                     latch.value(1)
                     latch.value(0)
+
+            # set new trigger timer
+            year, month, day, hour, minute, second, *_ = rtc.now()
+
+            # update rtc at 04:00
+            if hour == 4 and minute == 0 and second == 0:
+                rtc = settime(timezone_offset=TIMEZONE_OFFSET)
     except:
         # all off
+        latch.value(0)
         spi.write(OFF)
         # update LEDs
         latch.value(1)
@@ -75,5 +68,5 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
-    pass
+    main()
+    # pass
